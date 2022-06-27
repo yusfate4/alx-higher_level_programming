a5804a51b414e94964b2e7cae9fc9facd4f6ae45
Dahud Yusuf Ishola <65573250+yusfate4@users.noreply.github.com>
2022-06-27 21:32:56 +0100

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is the '0-rectangle' module
+Created on Mon 27 2022
+@author: Olanike Ogundiran
+"""
+
+
+class Rectangle():
+    """Empty Class Rectangle that defines a has no method"""
+    pass
